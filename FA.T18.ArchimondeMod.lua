c9e8001bbb28f97376aec25ce6061291b52b6080
Maïckel Pasta <Mackel Pasta>
2016-04-27 00:38:08 +0100

    Fix variable names

--- FA.T18.ArchimondeMod.lua
+++ FA.T18.ArchimondeMod.lua
@@ -2,6 +2,7 @@ local Hud           = FS.Hud
 local Encounters    = FS.Encounters
 local LSM           = LibStub:GetLibrary("LibSharedMedia-3.0")
 
+print("|cfffff569Archimonde Module : Loaded")
 -------------------------------------------------------------------------------
 -- SETTINGS
 -------------------------------------------------------------------------------
@@ -42,13 +43,13 @@ local MARK_TIMERS = {
   [11]  = 4
 }
 
-local DOOMFIRE      = {}
+local DOOMFIRES      = {}
 
 local TEXT_ARGS     = {
-    font    = SETTINGS.font, 
-    size    = SETTINGS.font_size, 
-    outline = SETTINGS.font_outline,
-    offset  = {0,15}
+  font    = SETTINGS.font, 
+  size    = SETTINGS.font_size, 
+  outline = SETTINGS.font_outline,
+  offset  = {0,15}
 }
 -------------------------------------------------------------------------------
 -- MODULE
@@ -67,6 +68,7 @@ local OPTIONS = mod:Options(_P) {
 function mod:OnEngage ( id , name , difficulty , size )
   for k, v in pairs(OPTIONS) do
     if OPTIONS[k] then
+      print (k)
       mod:CombatLog("SPELL_AURA_APPLIED", k, v)
       mod:CombatLog("SPELL_AURA_REMOVED", "Removed", v)
     end
@@ -79,43 +81,40 @@ end
 
 function mod:FocusedChaos ( _, _, args )
   local own = IsPlayer (args)
-  local key = args.destGUID .. args.spellId
-  Hud:RemovePoint(key)
-
-  local pt = Hud:CreateShadowPoint(args.destGUID, key)
-
+  Hud:RemovePoint(args.destKey)
+  local pt = Hud:CreateShadowPoint(args.destGUID, args.destKey)
+  
   function pt:Position()
-      local vx, vy = Hud:Vector(args.sourceGUID, args.destGUID, SETTINGS.overrun)
-      local x, y = self.ref:Position()
-      return x + vx, y + vy
+    local vx, vy = Hud:Vector(args.sourceGUID, args.destGUID, SETTINGS.overrun)
+    local x, y = self.ref:Position()
+    return x + vx, y + vy
   end
   local line = Hud:DrawLine(args.sourceGUID, pt, SETTINGS.ray_width)
-
+  
   function line:OnUpdate()
-      if own then
-          self:SetColor(unpack(SETTINGS.selfColor))
-      elseif self:UnitDistance("player") < 2 then
-          self:SetColor(unpack(SETTINGS.inColor))
-      else
-          self:SetColor(unpack(SETTINGS.outColor))
-      end
+    if own then
+      self:SetColor(unpack(SETTINGS.selfColor))
+    elseif self:UnitDistance("player") < 2 then
+      self:SetColor(unpack(SETTINGS.inColor))
+    else
+      self:SetColor(unpack(SETTINGS.outColor))
+    end
   end
 end
 
 function mod:ShackledTorment ( _, _, args )
   local own = IsPlayer (args)
-  local key = args.destGUID .. args.spellId
-  Hud:RemovePoint(key)
-  local root = Hud:CreateSnapshotPoint (args.destGUID, key)
+  Hud:RemovePoint(args.destKey)
+  local root = Hud:CreateSnapshotPoint (args.destGUID, args.destKey)
   local aera = Hud:DrawArea(root, 25)
   function aera:OnUpdate()
-      if own then
-          self:SetColor(unpack(SETTINGS.selfColor))
-      elseif self:UnitIsInside("player") then
-          self:SetColor(unpack(SETTINGS.inColor))
-      else
-          self:SetColor(unpack(SETTINGS.outColor))
-      end
+    if own then
+      self:SetColor(unpack(SETTINGS.selfColor))
+    elseif self:UnitIsInside("player") then
+      self:SetColor(unpack(SETTINGS.inColor))
+    else
+      self:SetColor(unpack(SETTINGS.outColor))
+    end
   end
   local line = Hud:DrawLine(root, args.destGUID, SETTINGS.shackled_width)
   line:SetColor (unpack(SETTINGS.defaultColor))
@@ -123,47 +122,42 @@ function mod:ShackledTorment ( _, _, args )
 end
 
 function mod:MarkLegion ( _, _, args )
-  local key = args.destGUID .. args.spellId
-  Hud:RemovePoint(key)
+  Hud:RemovePoint(args.destKey)
   local duration = select(6, UnitDebuff(args.destName, args.spellName))
   local idx = MARK_TIMERS[duration]
-
-  local pt = Hud:CreateShadowPoint(args.destGUID, key)
+  
+  local pt = Hud:CreateShadowPoint(args.destGUID, args.destKey)
   local timer = Hud:DrawTimer(pt, 10, duration)
-
+  
   if idx then
     timer:SetMarkerColor(idx, 0.5)
   else
     timer:SetColor(unpack(SETTINGS.defaultColor))
   end
-
+  
   local color = FS:GetClassColor(args.destName)
-  local text = Hud:DrawText(pt, "|c" .. color .. args.destName, TEXT_ARGS)
-
+  local text = Hud:DrawText(pt, "|c" .. color .. args.destName, TEXT_ARGS) 
 end
 
 function mod:Doomfire ( _, _, args )
-  local key = args.destGUID .. args.spellId
-  Hud:RemovePoint(key)
-  local pt = Hud:CreateShadowPoint(args.destGUID, key)
+  Hud:RemovePoint(args.destKey)
+  local pt = Hud:CreateShadowPoint(args.destGUID, args.destKey)
   local color = FS:GetClassColor(args.destName)
   local text = Hud:DrawText(pt, "|c" .. color .. args.destName, TEXT_ARGS)
   local timer = Hud:DrawTimer(pt, 10, 12)
   timer:SetColor(unpack(SETTINGS.defaultColor))
-  DOOMFIRE[key] = timer
+  DOOMFIRES[args.destKey] = timer
 end
 
 function mod:DoomfireDose ( _, _, args )
-  local key = args.destGUID .. args.spellId
-  if DOOMFIRE[key] then 
-    DOOMFIRE[key]:Reset(12)
+  if DOOMFIRES[args.destKey] then 
+    DOOMFIRES[args.destKey]:Reset(12)
   end
 end
 
 function mod:Shadowfel ( _, _, args )
-  local key = args.destGUID .. args.spellId
-  Hud:RemovePoint(key)
-  local pt = Hud:CreateShadowPoint(args.destGUID, key)
+  Hud:RemovePoint(args.destKey)
+  local pt = Hud:CreateShadowPoint(args.destGUID, args.destKey)
   local aera = Hud:DrawArea(pt, 8)
   aera:SetColor(unpack(SETTINGS.defaultColor))
   local color = FS:GetClassColor(args.destName)
@@ -171,6 +165,5 @@ function mod:Shadowfel ( _, _, args )
 end
 
 function mod:Removed ( _, _, args )
-  local key = args.destGUID .. args.spellId
-  Hud:RemovePoint(key)
+  Hud:RemovePoint(args.destKey)
 end
